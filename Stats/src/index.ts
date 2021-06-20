1793fe302323720d60491a06c70060c211cdb7c7
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2021-06-20 23:53:59 +0300

    seconds refactor approach

--- Stats/src/index.ts
+++ Stats/src/index.ts
@@ -1,13 +1,9 @@
-import { CsvFileReader } from "./CsvFileReader"
+import { MatchResult } from "./MatchResult"
+import { MatchReader } from "./MatchReader"
 
-const reader = new CsvFileReader("football.csv")
+const reader = new MatchReader("football.csv")
 reader.read()
 
-enum MatchResult {
-    HomeWin = "H",
-    AwayWin = "A",
-    Draw = "D"
-}
 let manUnitedWins = 0
 
 for (let match of reader.data) {

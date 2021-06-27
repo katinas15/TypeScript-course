b3e70810a8ebe7a902376c05bbb0b868a3d5456c
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2021-06-28 00:36:10 +0300

    Stats section

--- Stats/src/index.ts
+++ Stats/src/index.ts
@@ -1,17 +1,9 @@
-import { MatchResult } from "./MatchResult"
 import { MatchReader } from "./MatchReader"
+import { Summary } from "./Summary"
 
-const reader = new MatchReader("football.csv")
-reader.read()
+const matchReader = MatchReader.fromCsv("football.csv")
+matchReader.load()
 
-let manUnitedWins = 0
+const summary = Summary.winsAnalysisWithHtmlReport("Man United")
 
-for (let match of reader.data) {
-    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++
-    } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++
-    }
-}
-
-console.log(`Man United won ${manUnitedWins} games`)
+summary.buildAndPrintReport(matchReader.matches)
